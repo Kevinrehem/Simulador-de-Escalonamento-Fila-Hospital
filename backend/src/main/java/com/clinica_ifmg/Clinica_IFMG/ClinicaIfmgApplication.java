50413db4398ff727a98087a036aa3e684e57e6a6
Kevinrehem <kevin.rehem@gmail.com>
2025-10-31 21:56:19 -0300

    Base do código adicionada ao projeto

---
+++ backend/src/main/java/com/clinica_ifmg/Clinica_IFMG/ClinicaIfmgApplication.java
@@ -0,0 +1,13 @@
+package com.clinica_ifmg.Clinica_IFMG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaIfmgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaIfmgApplication.class, args);
+	}
+
+}
